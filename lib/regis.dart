19f4e304075f312e0774f4b08ca696e3447a06d4
wctn <6206022610068@fitm.kmutnb.ac.th>
2022-02-11 00:48:37 +0700

    ..

---
+++ lib/regis.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class Register extends StatelessWidget {
+  String title;
+  Color colortext;
+  String img;
+
+  Register(this.img, this.title, this.colortext);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+        focusColor: Colors.black,
+        border: const OutlineInputBorder(),
+        fillColor: Colors.pink,
+        labelText: title,
+        labelStyle:
+            MaterialStateTextStyle.resolveWith((Set<MaterialState> states) {
+          final Color color = states.contains(MaterialState.error)
+              ? Theme.of(context).errorColor
+              : Colors.blueGrey[200]!;
+
+          return TextStyle(color: color, letterSpacing: 1.3);
+        }),
+      ),
+      validator: (String? value) {
+        if (value == null || value == '') {}
+        return null;
+      },
+      autovalidateMode: AutovalidateMode.always,
+    );
+  }
+}
